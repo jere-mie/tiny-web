8f9a5e7c28f285635f7d493e0518995722ab7226
jere-mie <jere-mie@users.noreply.github.com>
2020-11-05 19:20:44 -0500

    added canvas

---
+++ canvas/script.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+ctx.fillStyle = "black";
+ctx.fillRect(100,100,100,100);
+ctx.fillRect(400,100,100,100);
+ctx.fillRect(250,250,100,100);
+ctx.fillRect(100,400,100,100);
+ctx.fillRect(400,400,100,100);
+
