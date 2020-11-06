f339b8a141b32c072a909041770ee4dff50c4743
jere-mie <jere-mie@users.noreply.github.com>
2020-11-06 00:19:17 -0500

    clear function works

--- canvas/script.js
+++ canvas/script.js
@@ -7,7 +7,7 @@ const ctx = canvas.getContext('2d');
 // ctx.fillRect(100,400,100,100);
 // ctx.fillRect(400,400,100,100);
 
-function draw(){
+const draw = function(){
     let x = parseInt(document.getElementById('x').value);
     let y = parseInt(document.getElementById('y').value);
     let w = parseInt(document.getElementById('width').value);
@@ -15,4 +15,13 @@ function draw(){
     let color = document.getElementById('color').value;
     ctx.fillStyle = color;
     ctx.fillRect(x,y,w,h);
+}
+
+const clearCanvas = function(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,512,512);
+}
+
+let hello = function(){
+    console.log('hell');
 }
