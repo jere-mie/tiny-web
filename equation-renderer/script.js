dc67b846046c750f760317f0f0376d7425a8ec95
jere-mie <jere-mie@users.noreply.github.com>
2020-12-22 01:06:42 -0500

    dragging elements works!!!

--- equation-renderer/script.js
+++ equation-renderer/script.js
@@ -1,3 +1,18 @@
+const container = document.getElementById('result');
+
+container.addEventListener('dragover', e => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(container, e.clientY);
+    const draggable = document.querySelector('.dragging');
+    if(afterElement == null){
+        console.log('get naenaed');
+        container.appendChild(draggable);
+    }else{
+        console.log('hello');
+        container.insertBefore(draggable, afterElement);
+    }
+})
+
 var i = 0;
 function run(){
     let eqn = document.querySelector("#eqn"); 
@@ -18,6 +33,17 @@ function run(){
     katex.render(eqn.value, rendered, {
         throwOnError: false
     });
+    item.setAttribute("class", "draggable");
+    item.setAttribute("draggable", "true");
+
+    item.addEventListener('dragstart', () => {
+        item.classList.add('dragging');
+    })
+
+    item.addEventListener('dragend', () => {
+        item.classList.remove('dragging');
+    })
+
     item.appendChild(del);
     item.appendChild(rendered);
     result.appendChild(item);
@@ -27,4 +53,18 @@ function run(){
 function delLine(index){
     document.querySelector("#ren"+index).remove();
     document.querySelector("#del"+index).remove();
+}
+
+function getDragAfterElement(container, y){
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if(offset < 0 && offset>closest.offset){
+            return {offset: offset, element: child};
+        }else{
+            return closest;
+        }
+    }, {offset: Number.NEGATIVE_INFINITY}).element;
 }
