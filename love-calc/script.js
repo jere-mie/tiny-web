aa7f686a46591ec317ab34897657cf347266c653
jere-mie <jere-mie@users.noreply.github.com>
2022-04-03 23:13:50 -0400

    improved some styling in a bunch of places

--- love-calc/script.js
+++ love-calc/script.js
@@ -5,9 +5,10 @@ function stoint(str){
 function lovify(){
     let name1 = document.getElementById('name1').value;
     let name2 = document.getElementById('name2').value;
+    let reg = /[^a-zA-Z0-9]/ig;
     console.log(name1+', '+name2);
-    let n1 = stoint(name1);
-    let n2 = stoint(name2);
+    let n1 = stoint(name1.replaceAll(reg, ''));
+    let n2 = stoint(name2.replaceAll(reg, ''));
     let diff = (n1*n2) % 101;
     console.log(n1+', '+n2);
     console.log(diff);
