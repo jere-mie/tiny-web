2c761f57ea7c4e48ef482906d0a691942ab5cc32
Jeremie Bornais <jeremiedevelops@gmail.com>
2022-06-09 20:59:30 +0000

    updated help menu and reorganized commands in terminal

--- terminal/html.js
+++ terminal/html.js
@@ -4,34 +4,54 @@ const helpTableTxt = `
 <p>Check out some info on the available commands in the table below</p>
 <br/>
 <table id="helpTable">
-<tr>
-<th>Command</th>
-<th>Description</th>
-</tr>
-<tr>
-<td>ping</td>
-<td>Displays 'pong' in the console</td>
-</tr>
-<tr>
-<td>deez</td>
-<td>Displays 'nuts' in the console</td>
-</tr>
-<tr>
-<td>help</td>
-<td>Displays a table with info on common commands</td>
-</tr>
-<tr>
-<td>clear (or cls)</td>
-<td>Clears the terminal</td>
-</tr>
-<tr>
-<td>echo</td>
-<td>Echoes back whatever you type in</td>
-</tr>
-<tr>
-<td>mozzarella</td>
-<td>Displays mozzarella sticks in the terminal</td>
-</tr>
+  <tr>
+    <th>Command</th>
+    <th>Description</th>
+  </tr>
+  <tr>
+    <td>ping</td>
+    <td>Displays 'pong' in the console</td>
+  </tr>
+  <tr>
+    <td>deez</td>
+    <td>Displays 'nuts' in the console</td>
+  </tr>
+  <tr>
+    <td>help</td>
+    <td>Displays a table with info on common commands</td>
+  </tr>
+  <tr>
+    <td>clear (or cls)</td>
+    <td>Clears the terminal</td>
+  </tr>
+  <tr>
+    <td>echo</td>
+    <td>Echoes back whatever you type in</td>
+  </tr>
+  <tr>
+    <td>history</td>
+    <td>Displays the commands you used during your session</td>
+  </tr>
+  <tr>
+    <td>who</td>
+    <td>Displays nothing</td>
+  </tr>
+  <tr>
+    <td>whoami</td>
+    <td>Displays the text 'jeremie'</td>
+  </tr>
+  <tr>
+    <td>ls</td>
+    <td>Displays 'directories' you can 'cd' into</td>
+  </tr>
+  <tr>
+    <td>cd</td>
+    <td>Opens one of the 'directories' listed by 'ls'</td>
+  </tr>
+  <tr>
+    <td>mozzarella (or mozz)</td>
+    <td>Displays mozzarella sticks in the terminal. Optionally takes an numeric argument which specifies the width of the image</td>
+  </tr>
 </table>
 `
 
