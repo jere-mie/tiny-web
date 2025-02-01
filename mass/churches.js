4e16226ad509257157e818be04cda149c217616a
jere-mie <jeremiedevelops@gmail.com>
2025-01-31 23:27:33 -0500

    add v1 masses page

---
+++ mass/churches.js
@@ -0,0 +1,606 @@
+/**
+ * @type {{ name: string, address: string }[]}
+ */
+export const churches = [
+    {
+        name: "St. John the Baptist",
+        address: "225 Brock St, Amherstburg, ON N9V 2H3",
+        coordinates: [42.1031731983902, -83.10377441840853],
+        website: "http://www.ahcfop.ca/",
+        phone: "+15197365418",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "11:00 AM",
+            },
+        ]
+    },
+    {
+        name: "St. Anthony of Padua",
+        address: "120 Munger St W, Harrow, ON N0R 1G0",
+        coordinates: [42.0450467161691, -82.92163991422747],
+        website: "http://www.ahcfop.ca/",
+        phone: "+15197365418",
+        masses: [
+            {
+                day: "Sunday",
+                time: "9:00 AM",
+            },
+        ]
+    },
+    {
+        name: "St. Clement",
+        address: "9567 County Rd 11, McGregor, ON N0R 1J0",
+        coordinates: [42.15109901769604, -82.9683391541853],
+        website: "http://www.centralessexcatholic.dol.ca/",
+        phone: "+15197768483",
+        masses: [
+            {
+                day: "Sunday",
+                time: "9:45 AM",
+            },
+        ]
+    },
+    {
+        name: "Holy Name of Jesus",
+        address: "146 Talbot St S, Essex, ON N8M 1B4",
+        coordinates: [42.17767177005348, -82.81692040425582],
+        website: "http://www.centralessexcatholic.dol.ca/",
+        phone: "+15197768483",
+        masses: [
+            {
+                day: "Saturday",
+                time: "6:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "11:00 AM",
+            },
+        ]
+    },
+    {
+        name: "St. Mary's",
+        address: "12048 County Road 34, Maidstone, ON N0R 1K0",
+        coordinates: [42.21299343293598, -82.88664825815857],
+        website: "https://centralessexcatholic.dol.ca/",
+        phone: "+15197768483",
+        masses: [
+            {
+                day: "Sunday",
+                time: "8:30 AM",
+            },
+        ]
+    },
+    {
+        name: "St. John the Evangelist",
+        address: "1688 County Rd 46, North Woodslee, ON N0R 1V0",
+        coordinates: [42.21867518541124, -82.72187505850836],
+        website: "http://www.centralessexcatholic.dol.ca/",
+        phone: "+15197768483",
+        masses: [
+            {
+                day: "Saturday",
+                time: "4:30 PM",
+            },
+        ]
+    },
+    {
+        name: "St. Joseph",
+        address: "9399 N Townline Rd, Amherstburg, ON N9J 2W6",
+        coordinates: [42.189519909059804, -83.06943312061841],
+        website: "https://lasallerivercanardfamily.dol.ca/",
+        phone: "+15197348044",
+        masses: [
+            {
+                day: "Sunday",
+                time: "10:15 AM",
+                note: "French",
+            },
+            {
+                day: "Sunday",
+                time: "12:00 PM",
+            },
+        ]
+    },
+    {
+        name: "Sacred Heart",
+        address: "1425 Divine St, LaSalle, ON N9J 0C5",
+        coordinates: [42.23426419124218, -83.09940449349426],
+        website: "https://lasallerivercanardfamily.dol.ca/",
+        phone: "+15197348044",
+        masses: [
+            {
+                day: "Sunday",
+                time: "8:30 AM",
+            },
+        ]
+    },
+    {
+        name: "St. Paul's",
+        address: "5885 Malden Rd, Windsor, ON N9H 1S5",
+        coordinates: [42.2479688106628, -83.0609787084127],
+        website: "https://lasallerivercanardfamily.dol.ca/",
+        phone: "+15197348044",
+        masses: [
+            {
+                day: "Saturday",
+                time: "4:30 PM",
+            },
+        ]
+    },
+    {
+        name: "Our Lady of Mt Carmel",
+        address: "4401 Mt Royal Dr, Windsor, ON N9G 2C5",
+        coordinates: [42.24516583537427, -83.01091299754506],
+        website: "https://sw.dol.ca/",
+        phone: "+15199695292",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "9:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "11:00 AM",
+            },
+        ]
+    },
+    {
+        name: "Corpus Christi",
+        address: "1400 Cabana Road W, Windsor, ON N9G 1C4",
+        coordinates: [42.25731116258417, -83.02030908248975],
+        website: "https://sw.dol.ca/",
+        phone: "+15199695292",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "9:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "11:00 AM",
+            },
+        ]
+    },
+    {
+        name: "Our Lady of Perpetual Help",
+        address: "804 Grand Marais Rd E, Windsor, ON N8X 3H8",
+        coordinates: [42.2849433591335, -83.00167238469194],
+        website: "http://www.olph.dionet.org/",
+        phone: "+15192580910",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "10:30 AM",
+            },
+        ]
+    },
+    {
+        name: "Immaculate Heart / St. Michael's",
+        address: "2153 Parkwood Ave, Windsor, ON N8W 2H1",
+        coordinates: [42.29972863291421, -83.00530966144828],
+        website: "http://www.olph.dionet.org/",
+        phone: "+15192580910",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "12:30 PM",
+            },]
+    },
+    {
+        name: "St. Francis of Assisi",
+        address: "1701 Turner Rd, Windsor, ON N8W 3J9",
+        coordinates: [42.30731901219269, -82.99789304481237],
+        website: "http://www.olph.dionet.org/",
+        phone: "+15192580910",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+                note: "Croatian",
+            },
+            {
+                day: "Sunday",
+                time: "9:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "11:00 AM",
+                note: "Croatian",
+            },
+        ]
+    },
+    {
+        name: "Holy Trinity",
+        address: "1035 Ellis St E, Windsor, ON N8X 2J2",
+        coordinates: [42.30722354028487, -83.01395364615968],
+        website: "http://www.olph.dionet.org/",
+        phone: "+15192580910",
+        masses: [
+            {
+                day: "Sunday",
+                time: "9:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "10:30 AM",
+                note: "Polish",
+            },
+            {
+                day: "Sunday",
+                time: "12:30 PM",
+                note: "Polish",
+            }
+        ]
+    },
+    {
+        name: "Our Lady of Guadalupe / San Juan Diego",
+        address: "834 Raymo Rd, Windsor, ON N8Y 4A6",
+        coordinates: [42.32682297936954, -82.9764729264545],
+        website: "https://ncwcf.dol.ca/",
+        phone: "+15199484133",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "10:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "12:30 PM",
+            }
+        ]
+    },
+    {
+        name: "Most Precious Blood",
+        address: "1947 Meldrum Rd, Windsor, ON N8W 4E3",
+        coordinates: [42.305284761494576, -82.98417978965752],
+        website: "https://ncwcf.dol.ca/",
+        phone: "+15199484133",
+        masses: [
+            {
+                day: "Satuday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "11:00 AM",
+                note: "ASL Available",
+            },
+        ]
+    },
+    {
+        name: "St. Theresa's",
+        address: "1991 Norman Rd, Windsor, ON N8T 1S1",
+        coordinates: [42.30951889445054, -82.96769296082253],
+        website: "https://ncwcf.dol.ca/",
+        phone: "+15199484133",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "9:00 AM",
+            },
+        ]
+    },
+    {
+        name: "Sts. Cyril And Methodius",
+        address: "1532 Alexis Rd, Windsor, ON N8Y 4P2",
+        coordinates: [42.31219764826648, -82.99144678781312],
+        website: "https://ncwcf.dol.ca/",
+        phone: "+15199484133",
+        masses: [
+            {
+                day: "Sunday",
+                time: "11:30 AM",
+            },
+        ]
+    },
+    {
+        name: "Our Lady of the Assumption",
+        address: "2735 University Ave W, Windsor, ON N9C 2J9",
+        coordinates: [42.307642170135026, -83.0697539299434],
+        website: "https://assumptionparish.ca/",
+        phone: "+15192532493",
+        masses: [
+            {
+                day: "Sunday",
+                time: "9:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "11:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "7:30 PM",
+            }
+        ]
+    },
+    {
+        name: "Holy Name of Mary / St. Benedict",
+        address: "",
+        coordinates: [42.30670322593854, -83.05441174959954],
+        website: "http://www.windsorlatinmass.org/",
+        phone: "+15192532493",
+        masses: [
+            {
+                day: "Sunday",
+                time: "2:00 PM",
+                note: "Latin, High Mass",
+            },
+        ]
+    },
+    {
+        name: "St. Alphonsus",
+        address: "85 Park St E, Windsor, ON N9A 3A8",
+        coordinates: [42.31675069582113, -83.03654463266152],
+        website: "https://stalphonsuschurch.ca/",
+        phone: "+15192561849",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "12:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "2:00 PM",
+                note: "First Sunday of each month. Part of the St. Daniel community: https://stdaniel.ca/.",
+            },
+            {
+                day: "Sunday",
+                time: "5:30 PM",
+            },
+        ]
+    },
+    {
+        name: "St. Angela Merici",
+        address: "980 Louis Ave, Windsor, ON N9A 1X9",
+        coordinates: [42.31300380992711, -83.02398064917112],
+        website: "https://stangelamerici.ca/",
+        phone: "+15192545542",
+        masses: [
+            {
+                day: "Saturday",
+                time: "4:30 PM",
+                note: "Italian",
+            },
+            {
+                day: "Sunday",
+                time: "9:30 AM",
+                note: "Italian",
+            },
+            {
+                day: "Sunday",
+                time: "11:30 AM",
+            },
+        ]
+    },
+    {
+        name: "Our Lady of the Atonement",
+        address: "2940 Forest Glade Dr, Windsor, ON N8R 1L5",
+        coordinates: [42.31023094858403, -82.91485650749323],
+        website: "https://eastwindsorcatholic.dol.ca/our-lady-of-the-atonement",
+        phone: "+15197356023",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "8:30 AM",
+            },
+            {
+                day: "Sunday",
+                time: "10:30 AM",
+            },
+        ]
+    },
+    {
+        name: "St.John Vianney",
+        address: "385 Dieppe St, Windsor, ON N8S 3V3",
+        coordinates: [42.339106220959664, -82.93324151261794],
+        website: "http://eastwindsorcatholic.dol.ca/",
+        phone: "+15197356023",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "9:30 AM",
+            },
+            {
+                day: "Sunday",
+                time: "11:30 AM",
+                note: "Every 3rd Sunday of the month. Part of St. Anthony of Padua Hungarian Community",
+            },
+        ]
+    },
+    {
+        name: "Ste. Anne",
+        address: "12233 Tecumseh Rd E, Windsor, ON N8N 1M3",
+        coordinates: [42.31321590308795, -82.88586297779122],
+        website: "https://catholicfamily.ca/",
+        phone: "+15197352182",
+        masses: [
+            {
+                day: "Saturday",
+                time: "4:30 PM",
+                note: "French",
+            },
+            {
+                day: "Saturday",
+                time: "6:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "9:30 AM",
+            },
+            {
+                day: "Sunday",
+                time: "12:00 PM",
+            },
+        ]
+    },
+    {
+        name: "Good Shepherd",
+        address: "462 Advance Blvd, Tecumseh, ON N8N 0B7",
+        coordinates: [42.30458342938133, -82.8240648888868],
+        website: "http://catholicfamily.ca/",
+        phone: "+15197359517",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "8:30 AM",
+            },
+            {
+                day: "Sunday",
+                time: "11:15 AM",
+            },
+        ]
+    },
+    {
+        name: "St. Jerome",
+        address: "3739 Ypres Ave, Windsor, ON N8W 1S9",
+        coordinates: [42.302041303585106, -82.97696334732743],
+        website: "http://catholicfamily.ca/",
+        phone: "+15199486115",
+        masses: [
+            {
+                day: "Sunday",
+                time: "10:00 AM",
+                note: "French",
+            },
+        ]
+    },
+    {
+        name: "St. Simon & St. Jude",
+        address: "488 St Charles St, Belle River, ON N0R 1A0",
+        coordinates: [42.29169969993489, -82.70569148781395],
+        website: "http://catholicfamily.ca/",
+        phone: "+15197283381",
+        masses: [
+            {
+                day: "Saturday",
+                time: "4:30 PM",
+            },
+            {
+                day: "Sunday",
+                time: "9:00 AM",
+                note: "French, held once per month",
+            },
+            {
+                day: "Sunday",
+                time: "10:30 AM",
+            }
+        ]
+    },
+    {
+        name: "St. John de Brebeuf",
+        address: "67 Main St E, Kingsville, ON N9Y 1A1",
+        coordinates: [42.04199032710271, -82.73395802551383],
+        website: "https://esscatholic.ca/",
+        phone: "+5193262643",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "8:30 AM",
+            },
+        ]
+    },
+    {
+        name: "St. Michael's",
+        address: "29 Elliott St, Leamington, ON N8H 3M4",
+        coordinates: [42.05561574721691, -82.60650039002873],
+        website: "https://esscatholic.ca/",
+        phone: "+5193262643",
+        masses: [
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "10:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "3:00 PM",
+                note: "Spanish",
+            },
+            {
+                day: "Sunday",
+                time: "6:30 PM",
+            },
+        ]
+    },
+    {
+        name: "Our Lady of the Most Blessed Sacrament",
+        address: "22 Middleton Line, Wheatley, ON N0P 2P0",
+        coordinates: [42.09470433404797, -82.46520811424308],
+        website: "https://esscatholic.ca/",
+        phone: "+5193262643",
+        masses: [
+            {
+                day: "Sunday",
+                time: "11:00 AM",
+            },
+        ]
+    }
+];
+
+const empty = {
+    name: "",
+    address: "",
+    coordinates: [],
+    website: "",
+    phone: "",
+    masses: [
+        {
+            day: "",
+            time: "",
+            note: "",
+        },
+    ]
+}
